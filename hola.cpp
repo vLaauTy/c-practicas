4bf14a4f2a68c5a3820e2da67cee68d4f87b2474
vLaauTy <96405301+vLaauTy@users.noreply.github.com>
2024-05-15 17:25:51 -0300

    first commit

---
+++ hola.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    for (int i = 1; i < 11; i++)
+    {
+        int resultado = 2 * i;
+        cout << "2*" << i << "=" << resultado << "\n";
+    }
+
+    return 0;
+}
